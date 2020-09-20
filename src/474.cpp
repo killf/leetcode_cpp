5930d29d8ec3418899dcf8f7293ac838c2d0594d
killf <killf@foxmail.com>
2020-09-20 12:31:50 +0800

    update

---
+++ src/474.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+
+    void count(const string &s, int &zeros, int &ones) {
+        zeros = ones = 0;
+        for (std::size_t i = 0; i < s.size(); i++) {
+            if (s[i] == '1') ones++;
+            else zeros++;
+        }
+    }
+
+    int findMaxForm(vector<string> &strs, int m, int n) {
+        vector<vector<int>> result(m + 1, vector<int>(n + 1));
+
+        int zeros, ones;
+        for (const auto &s:strs) {
+            count(s, zeros, ones);
+            for (int r = m; r >= zeros; r--) {
+                for (int c = n; c >= ones; c--) {
+                    result[r][c] = max(1 + result[r - zeros][c - ones], result[r][c]);
+                }
+            }
+        }
+
+        return result[m][n];
+    }
+};
