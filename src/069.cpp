2112714a2585fc9654ac9ba25fadd5bf8e94fd80
killf <killf@foxmail.com>
2020-09-21 10:54:19 +0800

    update

--- src/069.cpp
+++ src/069.cpp
@@ -6,9 +6,11 @@ using namespace std;
 class Solution {
 public:
   int mySqrt(int c) {
-    float x = float(c), y;
+    if (c == 0)return 0;
+
+    double x = double(c), y;
     do {
-      y = x * x - float(c);
+      y = x * x - double(c);
       x = x - y / (2 * x);
     } while (y > 0.1);
     return int(x);
@@ -17,5 +19,5 @@ public:
 
 int main() {
   Solution solution;
-  cout << solution.mySqrt(88) << endl;
+  cout << solution.mySqrt(2147395599) << endl;
 }
