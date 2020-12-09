119041bedf1db6938b7da70f83f37b7d2e5f4ff6
killf <killf@foxmail.com>
2020-12-09 15:48:39 +0800

    update

--- src/160.cpp
+++ src/160.cpp
@@ -12,20 +12,36 @@ struct ListNode {
   ListNode(int x) : val(x), next(nullptr) {}
 };
 
+//class Solution {
+//public:
+//  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//    int la = 0, lb = 0;
+//    for (auto *a = headA; a; a = a->next)la++;
+//    for (auto *b = headB; b; b = b->next)lb++;
+//
+//    for (; la > lb; headA = headA->next)la--;
+//    for (; lb > la; headB = headB->next)lb--;
+//
+//    while (headA && headB) {
+//      if (headA == headB)return headA;
+//      headA = headA->next;
+//      headB = headB->next;
+//    }
+//
+//    return nullptr;
+//  }
+//};
+
 class Solution {
 public:
   ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-    int la = 0, lb = 0;
-    for (auto *a = headA; a; a = a->next)la++;
-    for (auto *b = headB; b; b = b->next)lb++;
+    ListNode *a = headA, *b = headB;
 
-    for (; la > lb; headA = headA->next)la--;
-    for (; lb > la; headB = headB->next)lb--;
+    while (a || b) {
+      if (a == b)return a;
 
-    while (headA && headB) {
-      if (headA == headB)return headA;
-      headA = headA->next;
-      headB = headB->next;
+      if (a) a = a->next; else a = headB;
+      if (b)b = b->next; else b = headA;
     }
 
     return nullptr;
