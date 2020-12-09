82888aacb4edca7bcec58a4d4a1d75db02f041a7
killf@foxmail.com <rp302210015>
2020-12-09 21:31:15 +0800

    update

--- src/202.cpp
+++ src/202.cpp
@@ -9,34 +9,62 @@ using namespace std;
 
 class Solution {
 public:
-  int calc(int n) {
-    int sum = 0;
-    while (n >= 10) {
-      int v = n % 10;
-      sum += v * v;
-      n = n / 10;
+    int next(int n) {
+        int sum = 0;
+        while (n >= 10) {
+            int v = n % 10;
+            sum += v * v;
+            n = n / 10;
+        }
+        sum += n * n;
+        return sum;
     }
-    sum += n * n;
-    return sum;
-  }
 
-  bool isHappy(int n) {
-    if (n == 1)return true;
+    bool has_cycle(int i) {
+        return i == 4 || i == 16 || i == 20 || i == 37 || i == 42 || i == 58 || i == 89 || i == 145;
+    }
 
-
-
-
-  }
+    bool isHappy(int n) {
+        while (true){
+            if (n == 1)return true;
+            if (has_cycle(n))return false;
+            n = next(n);
+        }
+    }
 };
 
+vector<int> getHappyLists(Solution &solution, int n) {
+    if (n == 1)return {};
+
+    int a = n, b = n;
+    do {
+        a = solution.next(a);
+        b = solution.next(solution.next(b));
+        if (a == 1 || b == 1)return {};
+    } while (a != b);
+
+    vector<int> v;
+    do {
+        v.push_back(b);
+        b = solution.next(b);
+    } while (a != b);
+
+    sort(v.begin(), v.end());
+    return v;
+}
+
+void print(vector<int> &v) {
+    for (auto i:v) {
+        printf("%d ", i);
+    }
+    printf("\n");
+}
+
 int main() {
-  Solution solution;
-  printf("%d\n", solution.calc(9999));
-
-  map<int, int> m;
-  for (int i = 2; i <= 243; i++) {
-    m[i] = solution.calc(i);
-  }
-
-  return 0;
+    Solution solution;
+    for (int i = 2; i <= 243; i++) {
+        auto v = getHappyLists(solution, i);
+        print(v);
+    }
+    return 0;
 }
