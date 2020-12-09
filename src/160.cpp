586b4ca9a3ef2478aa06b7649b00cb5b049bfbfc
killf <killf@foxmail.com>
2020-12-09 15:39:02 +0800

    update

---
+++ src/160.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+struct ListNode {
+  int val;
+  ListNode *next;
+
+  ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    int la = 0, lb = 0;
+    for (auto *a = headA; a; a = a->next)la++;
+    for (auto *b = headB; b; b = b->next)lb++;
+
+    for (; la > lb; headA = headA->next)la--;
+    for (; lb > la; headB = headB->next)lb--;
+
+    while (headA && headB) {
+      if (headA == headB)return headA;
+      headA = headA->next;
+      headB = headB->next;
+    }
+
+    return nullptr;
+  }
+};
