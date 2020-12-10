e6070bc8859afcfc3d0a6dd8a724e631fa0e978a
killf@foxmail.com <rp302210015>
2020-12-10 21:06:14 +0800

    update

---
+++ src/237.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <hash_map>
+#include <hash_set>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode *node) {
+        while (node->next) {
+            node->val = node->next->val;
+            if (node->next->next)node = node->next;
+            else node->next = nullptr;
+        }
+    }
+};
