32b96a62c4f69f3aab39cb25c66ccaf123036f6c
killf@foxmail.com <rp302210015>
2020-12-10 20:36:35 +0800

    update

--- src/235.cpp
+++ src/235.cpp
@@ -12,30 +12,43 @@
 using namespace std;
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
+    int val;
+    TreeNode *left;
+    TreeNode *right;
 
-  explicit TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    explicit TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
-  TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
-    if (!root)return root;
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+        if (!root)return root;
 
-    TreeNode *node;
-    if (root->left && (node = lowestCommonAncestor(root->left, p, q)))return node;
-    if (root->right && (node = lowestCommonAncestor(root->right, p, q)))return node;
-  }
+        vector<TreeNode *> p1, p2;
+        if (!find(root, p, p1))return nullptr;
+        if (!find(root, q, p2))return nullptr;
 
-  TreeNode *find(TreeNode *root, TreeNode *p, TreeNode *q) {
-    if (!root) return root;
-    if (root == p || root == q)return root;
+        size_t size = min(p1.size(), p2.size());
+        for (size_t i = 0; i < size; i++) {
+            if (p1[i] != p2[i]) {
+                if (i == 0)return nullptr;
+                else return p1[i - 1];
+            }
+        }
 
-    TreeNode *node;
-    if (root->left && (node = find(root->left, p, q)))return node;
-    if (root->right && (node = find(root->right, p, q)))return node;
-    return nullptr;
-  }
+        return p1[size - 1];
+    }
+
+    bool find(TreeNode *root, TreeNode *p, vector<TreeNode *> &path) {
+        if (!root)return false;
+
+        path.push_back(root);
+        if (root == p) return true;
+
+        if (find(root->left, p, path)) return true;
+        if (find(root->right, p, path)) return true;
+
+        path.pop_back();
+        return false;
+    }
 };
