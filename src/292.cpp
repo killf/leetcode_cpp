707b3056603f8ea05776db321da7ee8eed7e7fdd
killf <killf@foxmail.com>
2020-12-11 09:21:17 +0800

    update

--- src/292.cpp
+++ src/292.cpp
@@ -13,8 +13,7 @@ using namespace std;
 
 class Solution {
 public:
-    bool canWinNim(int n) {
-        if (n <= 3)return true;
-        return !canWinNim(n - 1) || !canWinNim(n - 2) || !canWinNim(n - 3);
-    }
+  bool canWinNim(int n) {
+    return n % 4;
+  }
 };
