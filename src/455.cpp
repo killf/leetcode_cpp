6ca933beaff38149a71120bbec51ee16b6c0b8fe
killf@foxmail.com <rp302210015>
2020-12-12 09:09:21 +0800

    update

--- src/455.cpp
+++ src/455.cpp
@@ -16,7 +16,31 @@ using namespace std;
 
 class Solution {
 public:
-  int findContentChildren(vector<int>& g, vector<int>& s) {
+    int findContentChildren(vector<int> &g, vector<int> &s) {
+        if (g.empty() || s.empty())return 0;
 
-  }
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int num = 0, j = g.size() - 1;
+        for (int i = s.size() - 1; i >= 0 && j >= 0;) {
+            if (s[i] >= g[j]) {
+                num++;
+                i--;
+            }
+            j--;
+        }
+
+        return num;
+    }
 };
+
+int main() {
+    vector<int> g = {1, 2, 3};
+    vector<int> s = {1, 1};
+
+    Solution solution;
+    auto ret = solution.findContentChildren(g, s);
+
+    return 0;
+}
