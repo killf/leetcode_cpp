f265cf5a42a1a4133c318fd2de0c69fa497cbb07
killf@foxmail.com <rp302210015>
2020-12-12 18:21:41 +0800

    update

---
+++ src/482.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <hash_map>
+#include <hash_set>
+#include <queue>
+#include <stack>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    string licenseKeyFormatting(string S, int K) {
+
+    }
+};
