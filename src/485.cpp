b9d4ec2c0715bca08a44b7ed6a992132b524b46f
killf@foxmail.com <rp302210015>
2020-12-13 23:15:33 +0800

    update

--- src/485.cpp
+++ src/485.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <sstream>
-#include <algorithm>
-#include <map>
-#include <set>
-#include <hash_map>
-#include <hash_set>
-#include <queue>
-#include <stack>
-#include <iomanip>
-#include <cmath>
 
 using namespace std;
 
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-
+        return 0;
     }
 };
 
