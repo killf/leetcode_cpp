e5f771063d8cad382849bca29ba947ee0f19ee8c
killf <killf@foxmail.com>
2020-12-14 14:25:59 +0800

    update

--- src/541.cpp
+++ src/541.cpp
@@ -12,10 +12,7 @@ class Solution {
 public:
   string reverseStr(string s, int k) {
     int i = 0, step = 2 * k, end = s.size() - 1;
-    while (i + step < end) {
-      reverse(s, i, k);
-      i += step;
-    }
+    for (; i + step < end; i += step)reverse(s, i, k);
 
     if (end - i + 1 <= k) reverse(s, i, end - i + 1); else reverse(s, i, k);
     return s;
