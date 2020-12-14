3b2539b366a7476c731100c8eaa40a9317a7c924
killf <killf@foxmail.com>
2020-12-14 18:36:37 +0800

    update

---
+++ src/605.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <hash_map>
+#include <map>
+#include <sstream>
+#include <type_traits>
+
+using namespace std;
+
+class Solution {
+public:
+  bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+
+  }
+};
