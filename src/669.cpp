d18d12dd409afd80135d72639204c2c2063aa809
killf <killf@foxmail.com>
2020-12-15 14:16:16 +0800

    update

--- src/669.cpp
+++ src/669.cpp
@@ -28,11 +28,13 @@ struct TreeNode {
 class Solution {
 public:
   TreeNode *trimBST(TreeNode *root, int low, int high) {
-    if (!root || root->val < low || root->val > high)return nullptr;
+    if (!root)return nullptr;
+
+    if (root->val < low) return trimBST(root->right, low, high);
+    if (root->val > high)return trimBST(root->left, low, high);
 
     root->left = trimBST(root->left, low, high);
     root->right = trimBST(root->right, low, high);
-
     return root;
   }
 };
