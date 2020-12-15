312214ee642c699561291a35e772145bdad1ff45
killf <killf@foxmail.com>
2020-12-15 10:11:44 +0800

    update

--- src/605.cpp
+++ src/605.cpp
@@ -11,7 +11,36 @@ using namespace std;
 
 class Solution {
 public:
-  bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+  bool canPlaceFlowers(vector<int> &flowerbed, int n) {
+    int sum = 0, m = 0, size = flowerbed.size();
 
+    vector<int> nums;
+    for (int i = 0, j = 0; j < size; i = ++j) {
+      while (i < size && flowerbed[i] == 1)j = ++i;
+      while (j < size && flowerbed[j] == 0)j++;
+      int d = j - i;
+      if (d > m)m = d;
+      nums.push_back(d);
+    }
+
+    if (flowerbed[0] == 0)nums[0] += 1;
+    if (flowerbed[size - 1] == 0)nums[nums.size() - 1] += 1;
+
+    vector<int> flowers(max(m, 3) + 3);
+    flowers[0] = flowers[1] = flowers[2] = 0;
+    for (int i = 3; i < flowers.size(); i++)flowers[i] = flowers[i - 2] + 1;
+
+    for (auto c:nums)if ((sum += flowers[c]) >= n)return true;
+    return false;
   }
 };
+
+int main() {
+  vector<int> flowerbed = {0, 0, 0};
+  int n = 1;
+
+  Solution solution;
+  auto result = solution.canPlaceFlowers(flowerbed, n);
+
+  return 0;
+}
