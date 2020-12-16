4ec98c86394bebe40f5333f8bf0d38466b7e0ad6
killf@foxmail.com <rp302210015>
2020-12-16 19:45:53 +0800

    update

--- src/832.cpp
+++ src/832.cpp
@@ -16,25 +16,22 @@ using namespace std;
 
 class Solution {
 public:
-  vector<vector<int>> flipAndInvertImage(vector<vector<int>> &A) {
-    int row = A.size(), col = A[0].size();
-    for (int i = 0; i < row; i++) {
-      for (int j = 0; j <= col / 2; j++) {
-        swap(A[i][j], A[i][col - j - 1]);
-        A[i][j] = 1 - A[i][j];
-        if (j != col - j - 1)A[i][col - j - 1] = 1 - A[i][col - j - 1];
-      }
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>> &A) {
+        int row = A.size(), col = A[0].size();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col / 2; j++)swap(A[i][j], A[i][col - j - 1]);
+            for (int j = 0; j < col; j++)A[i][j] = 1 - A[i][j];
+        }
+        return A;
     }
-    return A;
-  }
 };
 
 int main() {
-  vector<vector<int>> A = {{1, 1, 0, 0},
-                           {1, 0, 0, 1},
-                           {0, 1, 1, 1},
-                           {1, 0, 1, 0}};
-  Solution solution;
-  solution.flipAndInvertImage(A);
-  return 0;
+    vector<vector<int>> A = {{1, 1, 0, 0},
+                             {1, 0, 0, 1},
+                             {0, 1, 1, 1},
+                             {1, 0, 1, 0}};
+    Solution solution;
+    solution.flipAndInvertImage(A);
+    return 0;
 }
