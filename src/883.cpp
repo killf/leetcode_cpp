e95f63e814365b9485953604ce0a51b59f69a6db
killf <killf@foxmail.com>
2020-12-17 09:12:06 +0800

    update

--- src/883.cpp
+++ src/883.cpp
@@ -16,7 +16,31 @@ using namespace std;
 
 class Solution {
 public:
-    int projectionArea(vector<vector<int>>& grid) {
+  int projectionArea(vector<vector<int>> &grid) {
+    int row = grid.size(), col = grid[0].size(), s = 0;
 
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        if (grid[i][j] > 0)s++;
+      }
     }
+
+    for (int i = 0; i < row; i++) {
+      int m = 0;
+      for (int j = 0; j < col; j++) {
+        m = max(m, grid[i][j]);
+      }
+      s += m;
+    }
+
+    for (int j = 0; j < col; j++) {
+      int m = 0;
+      for (int i = 0; i < col; i++) {
+        m = max(m, grid[i][j]);
+      }
+      s += m;
+    }
+
+    return s;
+  }
 };
