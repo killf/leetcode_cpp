767fb2286f2bb3f06768405843cd0acd98257cc5
killf@foxmail.com <rp302210015>
2020-12-19 23:34:10 +0800

    update

--- src/942.cpp
+++ src/942.cpp
@@ -18,6 +18,10 @@ using namespace std;
 class Solution {
 public:
   vector<int> diStringMatch(string S) {
-
+    int a = 0, b = S.length();
+    vector<int> result;
+    for (auto &s:S) if (s == 'I') result.push_back(a++); else result.push_back(b--);
+    result.push_back(a);
+    return result;
   }
 };
