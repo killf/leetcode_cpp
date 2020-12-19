9218b8a1e21924686dc3b7fc2fac83639d19198b
killf@foxmail.com <rp302210015>
2020-12-19 23:05:26 +0800

    update

---
+++ src/938.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <hash_map>
+#include <map>
+#include <sstream>
+#include <type_traits>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <set>
+#include <list>
+#include <cstring>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  int rangeSumBST(TreeNode *root, int low, int high) {
+    if (!root)return 0;
+
+    int sum = 0;
+    if (root->val >= low && root->val <= high) sum += root->val;
+    sum += rangeSumBST(root->left, low, high);
+    sum += rangeSumBST(root->right, low, high);
+    return sum;
+  }
+};
