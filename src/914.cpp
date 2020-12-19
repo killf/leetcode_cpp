765c451faf95f6655fbb87938058bfc4a13a3823
killf@foxmail.com <rp302210015>
2020-12-19 21:32:10 +0800

    update

--- src/914.cpp
+++ src/914.cpp
@@ -13,6 +13,18 @@
 
 using namespace std;
 
+inline int common_divisor(int a, int b) {
+  if (a < b)swap(a, b);
+
+  while (b != 0) {
+    int c = a % b;
+    a = b;
+    b = c;
+  }
+
+  return a;
+}
+
 class HashSet {
 private:
   struct Node {
@@ -26,14 +38,27 @@ public:
     auto loc = hash(value) % 1024;
 
     for (auto *p = data[loc]; p; p = p->next) {
-      if(p->value==value){
+      if (p->value == value) {
         p->count++;
         return;
       }
-
     }
 
+    data[loc] = new Node{value, 1, data[loc]};
   }
+
+  vector<int> counts() {
+    vector<int> counts;
+
+    for (int i = 0; i < 1024; i++) {
+      for (auto *p = data[i]; p; p = p->next) {
+        counts.push_back(p->count);
+      }
+    }
+
+    return counts;
+  }
+
 
 private:
   hash<int> hash;
@@ -44,6 +69,16 @@ private:
 class Solution {
 public:
   bool hasGroupsSizeX(vector<int> &deck) {
+    HashSet hashSet;
+    for (auto d:deck)hashSet.insert(d);
 
+    auto counts = hashSet.counts();
+    int result = counts[0];
+
+    for (int i = 1; i < counts.size(); i++) {
+      result = common_divisor(result, counts[i]);
+    }
+
+    return result > 1;
   }
 };
