dc047d90c87959f0690d8fb7aceaa814d58187c0
killf <killf@foxmail.com>
2020-12-21 16:11:47 +0800

    update

--- src/1207.cpp
+++ src/1207.cpp
@@ -1,11 +1,11 @@
 #include "utils.h"
 
-/*计数问题*/
+/*计数问题->查表法加速*/
 class Solution {
 public:
   bool uniqueOccurrences(vector<int> &arr) {
     int d1[2001] = {};
-    for (int n:arr)d1[n]++;
+    for (int n:arr)d1[n + 1000]++;
 
     bitset<1000> d2;
     for (int i = 0; i < 2001; i++) {
