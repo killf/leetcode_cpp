5bad5056f1d3a648ad5cb7f26218370dbcfdabac
killf <killf@foxmail.com>
2020-12-21 16:11:02 +0800

    update

---
+++ src/1207.cpp
@@ -0,0 +1,20 @@
+#include "utils.h"
+
+/*计数问题*/
+class Solution {
+public:
+  bool uniqueOccurrences(vector<int> &arr) {
+    int d1[2001] = {};
+    for (int n:arr)d1[n]++;
+
+    bitset<1000> d2;
+    for (int i = 0; i < 2001; i++) {
+      if (d1[i] > 0) {
+        if (d2.test(d1[i]))return false;
+        d2.set(d1[i]);
+      }
+    }
+
+    return true;
+  }
+};
