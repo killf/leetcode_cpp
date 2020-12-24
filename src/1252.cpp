1cbcbe1f81c76d3599610c244aae8f1d5c4c88a1
killf <killf@foxmail.com>
2020-12-24 11:56:07 +0800

    update

--- src/1252.cpp
+++ src/1252.cpp
@@ -4,18 +4,14 @@
 class Solution {
 public:
   int oddCells(int n, int m, vector<vector<int>> &indices) {
-    int rows[n], cols[m];
-    memset(rows, 0, sizeof(int) * n);
-    memset(cols, 0, sizeof(int) * m);
-
-    for (auto &pos:indices) {
-      rows[pos[0]]++;
-      cols[pos[1]]++;
-    }
+    int data[100] = {};
+    int *rows = data, *cols = data + 50;
 
     int r = 0, c = 0;
-    for (int i = 0; i < n; i++)if (rows[i] & 1)r++;
-    for (int i = 0; i < m; i++)if (cols[i] & 1)c++;
+    for (auto &pos:indices) {
+      if (++rows[pos[0]] & 1)r++; else r--;
+      if (++cols[pos[1]] & 1)c++; else c--;
+    }
 
     return r * m + c * n - r * c * 2;
   }
