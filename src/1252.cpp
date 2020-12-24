07ea82c0bd9f159a0ca071a0a644cad584cd7c7b
killf <killf@foxmail.com>
2020-12-24 11:52:20 +0800

    update

--- src/1252.cpp
+++ src/1252.cpp
@@ -1,10 +1,29 @@
 #include "utils.h"
 
-
+/*计数问题*/
 class Solution {
 public:
-  int oddCells(int n, int m, vector<vector<int>>& indices) {
+  int oddCells(int n, int m, vector<vector<int>> &indices) {
+    int rows[n], cols[m];
+    memset(rows, 0, sizeof(int) * n);
+    memset(cols, 0, sizeof(int) * m);
 
+    for (auto &pos:indices) {
+      rows[pos[0]]++;
+      cols[pos[1]]++;
+    }
 
+    int r = 0, c = 0;
+    for (int i = 0; i < n; i++)if (rows[i] & 1)r++;
+    for (int i = 0; i < m; i++)if (cols[i] & 1)c++;
+
+    return r * m + c * n - r * c * 2;
   }
 };
+
+int main() {
+  int a[10];
+  int b = size(a);
+
+  return 0;
+}
