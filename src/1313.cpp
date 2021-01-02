70c1d7b0eb9cc0b1f2112d17f3ecee596a28950d
killf@foxmail.com <rp302210015>
2021-01-02 09:03:26 +0800

    update

--- src/1313.cpp
+++ src/1313.cpp
@@ -1,9 +1,13 @@
 #include "utils.h"
 
-/**/
+/*遍历问题*/
 class Solution {
 public:
-  vector<int> decompressRLElist(vector<int>& nums) {
-
+  vector<int> decompressRLElist(vector<int> &nums) {
+    vector<int> result;
+    for (int i = 0; i < nums.size(); i += 2) {
+      for (int j = 0; j < nums[i]; j++)result.emplace_back(nums[i + 1]);
+    }
+    return result;
   }
 };
